456eedc80ecb1434bbe866baef9c3e0016874694
Damion Vega <damionvega333@gmail.com>
2022-07-12 16:45:28 -0700

    Try sync in server file

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const { expect } = require("chai");
 async function createInstances() {
   try {
     const User = require("./models/user.js");
+
     const BlogPost = require("./models/blogPost.js");
+    await BlogPost.sync({
+      force: true,
+      logging: (log) => console.log("\nsync log", log),
+    });
 
     await User.create({ name: "Lewis Hamilton" });
     const blogPost = await BlogPost.create({ name: "Hello" });
     expect(blogPost.slug).to.equal(`${blogPost.name}-${blogPost.id}`);
+    await BlogPost.sync({
+      force: true,
+      logging: (log) => console.log("\nsync log", log),
+    });
   } catch (error) {
     throw error;
   }
